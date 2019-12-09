6ced807f38301c32c4f87e1c8d5cdad8c3af8593
Parker Lowrey <30353257+plowrey@users.noreply.github.com>
2019-12-08 18:32:36 -0600

    Adding Comments

--- Scratch.py
+++ Scratch.py
@@ -12,21 +12,24 @@ background_color = (0, 0, 0)
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Ship Fight Game')
 
+# loading ship image and scaling it to size
 shipImg = pygame.image.load(r'C:\Codebase\Pictures\spaceship.png')
 shipImg = pygame.transform.scale(shipImg, (100, 100))
 
+# loading background image and fitting to screen
 background = pygame.image.load(r'C:\Codebase\Pictures\background.jpg')
 background = pygame.transform.scale(background, (700, 500))
 window.blit(background, (0, 0))
 
-FPS = 30
+# clock makes sure your game doesnt run to fast
 clock = pygame.time.Clock()
 
+
 x, y = 300, 400
-MOVE_UP = 4
-MOVE_DOWN = 3
-MOVE_RIGHT = 2
-MOVE_LEFT = 1
+MOVE_UP = 1
+MOVE_DOWN = 2
+MOVE_RIGHT = 3
+MOVE_LEFT = 4
 direction = 0
 
 # loop to keep window open until quit
@@ -35,7 +38,7 @@ while True:
 
         if event.type == pygame.QUIT:
             exit()
-
+        # Keydown looks for user to press a key
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
                 direction = MOVE_LEFT
@@ -46,6 +49,7 @@ while True:
             elif event.key == K_DOWN:
                 direction = MOVE_DOWN
 
+    # moving the x or y of the ship depending on keyboard input
     if direction == MOVE_LEFT:
         x -= 0.05
     elif direction == MOVE_RIGHT:
@@ -55,6 +59,7 @@ while True:
     elif direction == MOVE_DOWN:
         y += 0.05
 
+    # blit is printing images to the window every frame.
     window.blit(shipImg, (x, y))
     pygame.display.update()
     pygame.display.flip()
